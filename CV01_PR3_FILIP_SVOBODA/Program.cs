c5bb60ea7f14c484a6dfc7a9a19dc6d91e218082
Filip Svoboda <st58611@upce.cz>
2021-02-24 22:10:13 +0100

    Přidejte soubory projektu.

---
+++ CV01_PR3_FILIP_SVOBODA/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CV01_PR3_FILIP_SVOBODA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Napiš Rodné Číslo bez /");
+            int Pohlaví = 0;
+            for (int i = 0; i<10;i++)
+            {
+                int Číslo = Console.Read();
+                if ((i == 2) && ((Číslo == 53) || (Číslo == 54))) 
+                {
+                    Pohlaví++;
+                }
+            }
+            if (Pohlaví == 0) 
+            {
+                Console.WriteLine("Rodné číslo je mužské.");
+            }
+            if (Pohlaví == 1) 
+            {
+                Console.WriteLine("Rodné číslo je ženské.");
+            }
+            //string Rodné_Číslo = Console.ReadLine();
+            //int RC = Int32.Parse(Rodné_Číslo);
+            //int[] Pole = RC;
+            //if () 
+            { 
+            
+            }
+        }
+    }
+}
